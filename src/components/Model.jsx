982f68bcad57b65e493a7cd0209a33483751793f
Rinkit Adhana <therinkit@gmail.com>
2024-10-21 02:48:43 +0530

    add

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,8 +1,25 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import ModelView from "./ModelView"
+import * as THREE from "three"
+import { useRef, useState } from "react"
 
 const Model = () => {
+  const [size, setSize] = useState("small")
+  const [model, setModel] = useState({
+    title: "i Phone 15 Pro in Natural Titanium",
+    color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
+    img: yellowImg,
+  })
+
+  const cameraControlSmall = useRef()
+  const cameraControlLarge = useRef()
+
+  const small = useRef(new THREE.Group())
+  const large = useRef(new THREE.Group())
+
+  const [smallRotation, setSmallRotation] = useState(0)
+  const [largeRotation, setLargeRotation] = useState(0)
   useGSAP(() => {
     gsap.to("#heading", { y: 0, opacity: 1 })
   }, [])
@@ -14,7 +31,24 @@ const Model = () => {
         </h1>
         <div className=" flex flex-col items-center mt-5">
           <div className=" w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-            <ModelView />
+            <ModelView
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
+              setRotationState={setSmallRotation}
+              item={model}
+              size={size}
+            />
+            <ModelView
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
+              setRotationState={setLargeRotation}
+              item={model}
+              size={size}
+            />
           </div>
         </div>
       </div>
