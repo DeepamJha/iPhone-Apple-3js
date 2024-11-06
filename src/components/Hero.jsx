670efff7e09f96f600aeb7b4e692313248eb2a3e
Rinkit Adhana <therinkit@gmail.com>
2024-11-06 15:35:30 +0530

    hero-updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,36 +1,36 @@
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { useEffect, useState } from "react";
-import { heroVideo, smallHeroVideo } from "./../utils/index";
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { useEffect, useState } from "react"
+import { heroVideo, smallHeroVideo } from "./../utils/index"
 
 const Hero = () => {
   const [videoSrc, setVideoSrc] = useState(
     window.innerWidth < 760 ? smallHeroVideo : heroVideo
-  );
+  )
   const handleVideoSrcSet = () => {
     if (window.innerWidth < 760) {
-      setVideoSrc(smallHeroVideo);
+      setVideoSrc(smallHeroVideo)
     } else {
-      setVideoSrc(heroVideo);
+      setVideoSrc(heroVideo)
     }
-  };
+  }
   useEffect(() => {
-    window.addEventListener("resize", handleVideoSrcSet);
+    window.addEventListener("resize", handleVideoSrcSet)
     return () => {
-      window.removeEventListener("resize", handleVideoSrcSet);
-    };
-  }, []);
+      window.removeEventListener("resize", handleVideoSrcSet)
+    }
+  }, [])
   useGSAP(() => {
-    gsap.to("#hero", { opacity: 1, delay: 2.0, duration: 2 });
-    gsap.to("#cta", { opacity: 1, delay: 2.0, y: -50, duration: 1 });
-  }, []);
+    gsap.to("#hero", { opacity: 1, delay: 2.0, duration: 2 })
+    gsap.to("#cta", { opacity: 1, delay: 2.0, y: -80, duration: 1 })
+  }, [])
   return (
-    <section className=" w-full nav-height bg-black relative">
-      <div className=" h-5/6 w-full flex-center flex-col">
-        <p id="hero" className=" hero-title font-SFproBold">
+    <section className=" w-full nav-height bg-black relative ">
+      <div className=" h-5/6 w-full flex-center flex-col ">
+        <p id="hero" className=" hero-title font-SFproBold border">
           iPhone 15 Pro
         </p>
-        <div className=" md:w-10/12 w-9/12">
+        <div className=" md:w-9/12 w-9/12 border">
           <video
             className=" pointer-events-none"
             autoPlay
@@ -42,8 +42,11 @@ const Hero = () => {
           </video>
         </div>
       </div>
-      <div id="cta" className="flex flex-col items-center opacity-0 ">
-        <a href="#highlights" className="btn">
+      <div id="cta" className="flex flex-col items-center opacity-0 border">
+        <a
+          href="https://www.apple.com/in/shop/buy-iphone/iphone-15"
+          className="btn"
+        >
           Buy
         </a>
         <p className="font-normal text-gray text-lg">
@@ -51,7 +54,7 @@ const Hero = () => {
         </p>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
